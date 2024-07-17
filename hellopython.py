adfc9ab3447549748f9f1d35bfe8d9c212d851f5
SuhanshBangre29 <suhansh.bangre@americanchase.com>
2024-07-17 21:32:11 +0530

    Added image files to directory

--- hellopython.py
+++ hellopython.py
@@ -2,4 +2,6 @@ print("Hello Suhansh from Python !")
 print("We are in American Chase now")
 print("We will be  working on python, salesforce, workday")
 
-print("We will be wroking in night shift 5-days-a-week")
+print("We will be working in night shift 5-days-a-week")
+
+print("We are in SDE team")
